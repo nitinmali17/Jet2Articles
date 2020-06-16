70e086c5e921ec73cd2e2486fadecf1d842669fc
nitinmali17 <malinitin50@gmail.com>
2020-06-16 13:29:18 +0530

    Added initial Ui and required libraries

--- app/src/main/java/com/nitinm/jet2articles/MainActivity.kt
+++ app/src/main/java/com/nitinm/jet2articles/MainActivity.kt
@@ -2,11 +2,26 @@ package com.nitinm.jet2articles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView(){
+        articlesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            this.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                }
+            })
+        }
     }
 }
